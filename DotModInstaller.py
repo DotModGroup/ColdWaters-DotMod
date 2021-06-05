f9ebde4c98f2c7e89fceffe085f3682f82e85470
HobbitJack <jruteg@gmail.com>
2021-06-05 19:14:28 -0400

    Installer update

--- DotModInstaller.py
+++ DotModInstaller.py
@@ -1,4 +1,4 @@
-#Personal Reminder: Compile with 'python -m nuitka --onefile --windows-onefile-tempdir --windows-icon-from-ico=DMEXE.ico DotModInstaller.py'
+#Personal Reminder: Compile with python -m nuitka --onefile --windows-onefile-tempdir --windows-icon-from-ico=DMEXE.ico DotModInstaller.py
 #------------
 
 #Imports
@@ -7,33 +7,26 @@
 import os
 #File tree copying and removal
 import shutil
+#Config (This installer is meant to be universal, for both the main mod and addons)
+import configparser
 
 #Good programming practices
 def main():
     user = os.getlogin()
-
-    #Welcome/Install instructions
-    print('Reminder: Only download DotMod from the GitHub page. Any other downloads are *unauthorized* and cannot be verified.')
-    print('If you did not download the mod\'s files from the GitHub, close this program at once and run a malware scan.')
-    print('\nThank you for choosing DotMod! This program will help you automatically install/update the mod.')
-    print('If you have JSGME installed, disable all mods currently enabled, and make sure the game is closed.')
-    #Delete old options to prevent starting bugs
-    delOptions = input('Have you used Epic Mod or any other mod that modifies the game\'s code since you last used DotMod? Y/N: ')
-    if os.path.exists(f'C:\\Users\\{user}\\AppData\\LocalLow\\Killerfish Games\\Cold Waters\\options.txt') and delOptions.lower() == 'y':
-        os.remove(f'C:\\Users\\{user}\\AppData\LocalLow\\Killerfish Games\\Cold Waters\\options.txt')
-        print('Please note that this has reset your options.') 
-
+    config = configparser.ConfigParser()
+    config.read('%s/Installer.ini' % (os.getcwd()))
+    modName = config['Settings']['modName']
+    #modName = 'DotMod'
+    mod = '%s/ColdWaters_Data' % (os.getcwd())
+    
     #Auto-locate; Simply checks likely locations for the game to be installed to       
     if os.path.exists('C:\Program Files (x86)\Steam\steamapps\common\Cold Waters'):
-        print('\nInstalling to Steam on drive C.\n')
         installDirectory = 'C:\Program Files (x86)\Steam\steamapps\common\Cold Waters'
         
     elif os.path.exists('D:\Steam\steamapps\common\Cold Waters'):
-        print('\nInstalling to Steam on drive D.\n')
         installDirectory = 'D:\Steam\steamapps\common\Cold Waters'
         
     elif os.path.exists('D:\Games\steamapps\common\Cold Waters'):
-        print('\nInstalling to Games on drive D.\n')
         installDirectory = 'D:\Games\steamapps\common\Cold Waters'
         
     else:
@@ -41,36 +34,62 @@ def main():
         installDirectory = input('Please input the directory of your Cold Waters install:')
 
     #Make sure everything's correct:
-    if input(f'This will install the mod to {installDirectory}. \nIs that correct? Y/N:').lower() != 'y':
+    if input(f'This will install {modName} to {installDirectory}. \nIs that correct? Y/N:').lower() != 'y':
         installDirectory = input('Please input the directory of your Cold Waters install:')
         if installDirectory == 'yourmom.com':
-            print('Stealing is bad, Epic')
+            print('Stealing is bad, Epic.')
+            input('Press Return to exit.')
+            return
+        
+    modTarget = installDirectory + f'\\MODS\\{modName}\ColdWaters_Data'
+    #modTarget = installDirectory
+        
+    if modName == 'DotMod':
+        #Welcome/Install instructions for the main mod
+        print('\nReminder: Only download DotMod from the GitHub page. Any other downloads are *unauthorized* and cannot be verified.')
+        print('If you did not download the mod\'s files from the GitHub, close this program at once and run a malware scan.')
+        print('\nThank you for choosing DotMod! This program will help you automatically install/update the mod.')
+        print('If you have JSGME installed, disable all mods currently enabled, and make sure the game is closed.')
+        
+        #Delete old options to prevent starting bugs
+        delOptions = input('Have you used Epic Mod or any other mod that modifies the game\'s code since you last used DotMod? Y/N: ')
+        if os.path.exists(f'C:\\Users\\{user}\\AppData\\LocalLow\\Killerfish Games\\Cold Waters\\options.txt') and delOptions.lower() == 'y':
+            os.remove(f'C:\\Users\\{user}\\AppData\LocalLow\\Killerfish Games\\Cold Waters\\options.txt')
+            print('Please note that this has reset your options.')
+            
+        #Install JSGME
+        JSGME = '%s/JSGME' % (installDirectory)
+        if os.path.exists(f'{installDirectory}\\JSGME.ini'):
+            print('JSGME already installed.')
 
-    #Install JSGME
-    JSGME = '%s/JSGME' % (installDirectory)
-    if os.path.exists(f'{installDirectory}\\JSGME.ini'):
-        print('JSGME already installed.')
+        else:
+            shutil.copytree(JSGME, installDirectory, dirs_exist_ok=True)
+            print('JSGME installed. Please read through JSGME Help.txt and JoneSoft.txt.')
 
-    else:
-        shutil.copytree(JSGME, installDirectory, dirs_exist_ok=True)
-        print('JSGME installed. Please read through JSGME Help.txt and JoneSoft.txt.')
+    #Remove the old files
+        if os.path.exists(f'{installDirectory}\\MODS\{modName}'):
+            shutil.rmtree(f'{installDirectory}\\MODS\{modName}', ignore_errors=True)
+            print('Previous version of the mod removed.')
 
-    #Install the mod to JSGME
-    mod = '%s/ColdWaters_Data' % (os.getcwd())
-    modTarget = installDirectory + '\\MODS\\DotMod\\ColdWaters_Data'
-    if os.path.exists(f'{installDirectory}\\MODS\DotMod'):
-        shutil.rmtree(f'{installDirectory}\\MODS\DotMod', ignore_errors=True)
-        print('Previous version of DotMod removed.')
-
+    #Install the mod
     print('Working, please wait...\nThis may take a minute.')
     shutil.copytree(mod, modTarget, dirs_exist_ok=True)
     print('Mod installed.')
 
     #And we're done!
-    print(f'\nThis program is completed. Now, navigate to your Cold Waters directory, at {installDirectory}, and run JSGME.exe (if you used Epic Mod, this may be called Epic Mod Install).')
-    print('Enable DotMod first, and then any addons you want over it.')
-    print('Thank you for using DotMod.')
-    input('Press Return to exit.')
+    if modName == 'DotMod':
+        print(f'\nThis program is completed. Now, navigate to your Cold Waters directory, at {installDirectory}, and run JSGME.exe (if you used Epic Mod, this may be called Epic Mod Install) and enable the main mod there.')
+        #print('This program is completed. Now, navigate to your Cold Waters directory, at %s, and run the game.')
+        print('Enable the main mod first, and then any addons you want over it.')
+        print('Thank you for using DotMod.')
+        input('Press Return to exit.')
+    else:
+        print(f'\nThis program is completed. Now, navigate to your Cold Waters directory, at {installDirectory}, and run JSGME.exe (if you used Epic Mod, this may be called Epic Mod Install) and enable this addon there.')
+        #print('This program is completed. Now, navigate to your Cold Waters directory, at %s, and run the game.')
+        print('The installation of this addon is complete. If you experience any crashes, please remove all addons and re-enable them.')
+        #print('The installation of this addon is complete. If you experience any crashes, please remove everything in the priority folder and reinstall.')
+        print('Please contact the addon\'s creator if the issue persists.')
+        input('Press Return to exit.')
 
 if __name__ == '__main__':
     main()
