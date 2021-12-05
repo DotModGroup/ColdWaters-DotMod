5258db43caeef5424091ffb8de6ac24bf9b751f0
HobbitJack <jruteg@gmail.com>
2021-12-04 22:33:32 -0500

    Update weaponsscript_public.py

--- Utilities/weaponsscript_public.py
+++ Utilities/weaponsscript_public.py
@@ -1,37 +1,45 @@
-file = open(input('Input the complete file directory of where your weapons.txt is located\n> '), encoding='utf8')
-save_location = input('Input the complete file directory of where your output should go.'
-                      ' Note that the directory must exist completely.\n> ')
+file = open(
+    input("Input the complete file directory of where your weapons.txt is located\n> "),
+    encoding="utf8",
+)
+save_location = input(
+    "Input the complete file directory of where your output should go."
+    " Note that the directory must exist completely.\n> "
+)
 
 weapon = None
 weapon_name = None
 weapon_type = None
 
 for line in file:
-    if line == '\n':
+    if line == "\n":
         continue
 
-    elif line.startswith('WeaponObjectReference'):
+    if line.startswith("WeaponObjectReference"):
         weapon_name = line[22:]
         weapon = line
-        weapon_type = 'weapon_'
+        weapon_type = "weapon_"
 
-    elif line.startswith('DepthWeaponObjectReference'):
+    elif line.startswith("DepthWeaponObjectReference"):
         weapon_name = line[27:]
         weapon = line
-        weapon_type = 'depthweapon_'
+        weapon_type = "depthweapon_"
 
-    elif line.startswith('CountermeasureName'):
+    elif line.startswith("CountermeasureName"):
         weapon = line
-        weapon_type = 'countermeasure_'
+        weapon_type = "countermeasure_"
 
-    elif line.startswith('CountermeasureObjectReference'):
+    elif line.startswith("CountermeasureObjectReference"):
         weapon_name = line[30:]
         weapon += line
 
-    elif line.startswith('[/Model]') and weapon is not None:
+    elif line.startswith("[/Model]") and weapon is not None:
         weapon += line
-        file_save = open('%s\\%s%s.txt'
-                         % (save_location, weapon_type, weapon_name[0:-1]), encoding='utf8', mode='a')
+        file_save = open(
+            "%s\\%s%s.txt" % (save_location, weapon_type, weapon_name[0:-1]),
+            encoding="utf8",
+            mode="a",
+        )
         file_save.write(weapon)
         file_save.close()
         weapon = None
@@ -40,4 +48,4 @@ for line in file:
     elif weapon is not None:
         weapon += line
 
-print('Program completed without errors.')
+print("Program completed without errors.")
