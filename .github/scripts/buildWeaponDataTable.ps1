253f948fbe646e543a688a9adc0e43c0e7bf7938
FirebarSim <32907623+FirebarSim@users.noreply.github.com>
2022-01-04 18:30:13 +0000

    Update buildWeaponDataTable.ps1

--- .github/scripts/buildWeaponDataTable.ps1
+++ .github/scripts/buildWeaponDataTable.ps1
@@ -5,6 +5,7 @@
     [String]$Description
 }
 
+$log = @()
 $data = @()
 
 Get-ChildItem "main/ColdWaters_Data/StreamingAssets/dotmod/weapons" -Filter weapon_*.txt -Recurse | 
@@ -23,21 +24,35 @@ Foreach-Object {
         }
     }
 
-    Copy-Item -Path "main/ColdWaters_Data/StreamingAssets/dotmod/hud/sprites/${weapon.Sprite}" -Destination "website/media/weapons/${weapon.Sprite}"
+    try{
+        $sprite = Get-Item -Path "main/ColdWaters_Data/StreamingAssets/dotmod/${weapon.Sprite}"
+        $sprite.CopyTo("website/media/weapons")
+    }
+    catch{
+        $log.Add("ERROR: Failed to copy Sprite for ${weapon.ObjectReference} from main/ColdWaters_Data/StreamingAssets/dotmod/${weapon.Sprite}")
+    }
 
-    $content = Get-Content "main/ColdWaters_Data/StreamingAssets/dotmod/language_en/weapon/${weapon.ObjectReference}_description.txt"
-    $lines = $content.Split("\n")
+    try{
+        $content = Get-Content "main/ColdWaters_Data/StreamingAssets/dotmod/language_en/weapon/${weapon.ObjectReference}_description.txt"
+        $lines = $content.Split("\n")
 
-    Foreach ($line in $lines) {
-        if ($line.StartsWith('WeaponName') ){
-            $weapon.ObjectReference = $line.Split("=")[1].Trim()
+        Foreach ($line in $lines) {
+            if ($line.StartsWith('WeaponName') ){
+                $weapon.ObjectReference = $line.Split("=")[1].Trim()
+            }
+            elseif ($line.StartsWith('WeaponDescription') ){
+                $weapon.ObjectReference = $line.Split("=")[1].Trim()
+            }
         }
-        elseif ($line.StartsWith('WeaponDescription') ){
-            $weapon.ObjectReference = $line.Split("=")[1].Trim()
-        }
+    }
+    catch{
+        $log.Add("ERROR: Failed to get Description for ${weapon.ObjectReference} from main/ColdWaters_Data/StreamingAssets/dotmod/language_en/weapon/${weapon.ObjectReference}_description.txt")
     }
 
     $data.Add($weapon)
+
 }
 
 ConvertTo-Json -InputObject $data -Depth 5 | Out-File ( New-Item -Path "website/content/weapons.json" -Force)
+
+$log | Out-File ( New-Item -Path "website/logs/buildWeaponDataTable.log" -Force)
